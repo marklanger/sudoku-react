062b427ce6936839990329dcc14a9d1d80de52f8
Mark Langer <7421898+marklanger@users.noreply.github.com>
2020-01-18 18:00:35 -0700

    Change cell component from class to function

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,23 +2,17 @@ import React from "react";
 
 import "./Cell.css";
 
-export default class Cell extends React.Component{
-  constructor(props){
-    super(props);
-  };
-
-  render() {
-    if(this.props.number !== 0){
-      return(
-      <div className="locked-cell" id={this.props.id}>
-      {this.props.number}
-      </div>
-      );
-    } else {
-      return(
-      <div className="playable-cell" id={this.props.id}>
-      </div>
-      );
-    };
+export default function Cell(props){
+  if(props.number !== 0){
+    return(
+    <div className="locked-cell" id={props.id}>
+    {props.number}
+    </div>
+    );
+  } else {
+    return(
+    <div className="playable-cell" id={props.id}>
+    </div>
+    );
   };
 };
