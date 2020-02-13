f19a5417373c413269e9c16133a323f4753f51cd
Mark Langer <7421898+marklanger@users.noreply.github.com>
2020-02-12 21:36:09 -0700

    Add tab movement and failure checker

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import boardBuilder from '../logic/boardBuilder.js';
 import solveBoard from '../logic/boardSolver.js';
+import { boardPassesTests } from '../logic/failureChecker.js';
 import Cell from './Cell.js'
 import { game1 } from '../logic/data.js'
 
@@ -14,7 +15,7 @@ export default class Board extends React.Component{
       toggled : ''
     };
     this.toggleCell = this.toggleCell.bind(this);
-    this.updateValue = this.updateValue.bind(this);
+    this.handleKey = this.handleKey.bind(this);
     this.solveBoard = this.solveBoard.bind(this);
   };
 
@@ -33,16 +34,19 @@ export default class Board extends React.Component{
     });
   };
 
-  updateValue(id, e){
+  handleKey(id, e){
     e.persist();
+    // if a number, update value of the cell to be the number
     if(/[1-9]/.test(e.key)){
       this.setState(prevState => {
 	return board[id].value = e.key;
       });
-    } else if (e.key === 'Delet' || e.key === 'Backspace'){
+    } else if (e.key === 'Delete' || e.key === 'Backspace'){
       this.setState(prevState => {
         return board[id].value = '';
       });
+    } else if (e.key === 'Enter') {
+      this.toggleCell(id, e);
     }
   };
 
@@ -53,25 +57,30 @@ export default class Board extends React.Component{
     )
   };
 
+  checkForFailure(){
+    alert(boardPassesTests(board));
+  }
+
   render(){
     return (
       <div className="sudoku-board">
-	{
-	  this.state.board.map((item) => {
-	    return(
-	      <Cell
-	        key={item.id.toString()}
-	        id={item.id}
-	        className={item.status + ' ' + item.border + ' ' + (item.toggled ? 'toggled' : '')}
-	        onClick={(e) => this.toggleCell(item.id, e)}
-	        tabIndex='0'
-	        onKeyDown={(e) => this.updateValue(item.id, e)}
-	        value={item.value}
-	      />
-	    );
-	  })
-	}
+      {
+	this.state.board.map((item) => {
+          return(
+            <Cell
+              key={item.id.toString()}
+              id={item.id}
+              className={item.status + ' ' + item.border + ' ' + (item.toggled ? 'toggled' : '')}
+              onClick={(e) => this.toggleCell(item.id, e)}
+              tabIndex={(item.id + 1).toString()}
+              onKeyDown={(e) => this.handleKey(item.id, e)}
+              value={item.value}
+            />
+          );
+        })
+      }
       <button onClick={() => this.solveBoard()}>Solve</button>
+      <button onClick={() => this.checkForFailure()}>Check</button>
       </div>
     );
   };
