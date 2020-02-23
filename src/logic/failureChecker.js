29b1a70cb8de553fc2fe446f8645370b60737224
Mark Langer <7421898+marklanger@users.noreply.github.com>
2020-02-22 22:42:57 -0700

    Use row, column, and square references to test for failure
    Created a reference object for the rows, columns, and squares that
    shows all the cells the in each applicable row, column, and square.
    This allows for one failure checking function (instead of one function
    for each row, column and square) that goes through the referenced
    cells to look for repeated values.

--- src/logic/failureChecker.js
+++ src/logic/failureChecker.js
@@ -1,47 +1,12 @@
-import { cellsInSquares } from './data.js';
+import { cellsInRows, cellsInColumns, cellsInSquares } from './data.js';
 
 function boardPassesTests(board){
-  return squaresDoNotFail(board) &&
-         rowsDoNotFail(board) && 
-         columnsDoNotFail(board)
+  return sectionDoesNotFail(board, cellsInRows) &&
+         sectionDoesNotFail(board, cellsInColumns) && 
+         sectionDoesNotFail(board, cellsInSquares)
 };
 
-function rowsDoNotFail(board){
-  // go through each row and see if any two values are the same
-  for(let row = 0; row < 9; row++){
-    let listOfUsedVals = [];
-    for(let cell = 0; cell < 9; cell++){
-      let id = row * 9 + cell;
-      if(listOfUsedVals.includes(board[id]['value'])){
-	return false;
-      }
-      if(board[id]['value'] !== ''){
-	listOfUsedVals.push(board[id]['value']);
-      };
-    };
-  };
-  return true;
-};
-
-function columnsDoNotFail(board){
-  let testBoard = {...board};
-  // go through each column and see if any two values are the same
-  for(let col = 0; col < 9; col++){
-    let listOfUsedVals = [];
-    for(let cell = 0; cell < 9; cell++){
-      let id = col + (cell * 9);
-      if(listOfUsedVals.includes(testBoard[id]['value'])){
-	return false;
-      }
-      if(testBoard[id]['value'] !== ''){
-	listOfUsedVals.push(testBoard[id]['value']);
-      };
-    };
-  };
-  return true;
-};
-
-function squaresDoNotFail(board){
+function sectionDoesNotFail(board, referenceCells){
   const refsForSquares = cellsInSquares;
   for (let squareId = 1; squareId < 10; squareId++){
     let listOfUsedVals = [];
